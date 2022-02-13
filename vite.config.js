208346e052d85cd463cf70840042443ccd7fa56d
Nikita Nitichevski <me@donnikitos.com>
2022-02-13 12:46:43 +0200

    updated vite library build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
-import styledLib from './vite-plugin-styledLib';
 
 export default defineConfig({
-	plugins: [react(), styledLib()],
+	plugins: [react()],
 	build: {
+		cssCodeSplit: true,
 		lib: {
-			entry: path.resolve(__dirname, 'src/components/Truncated/index.tsx'),
+			entry: path.resolve(
+				__dirname,
+				'src/components/Truncated/index.tsx',
+			),
+			formats: ['umd'],
 			name: 'react-component-truncated',
 			fileName: 'index',
 		},
